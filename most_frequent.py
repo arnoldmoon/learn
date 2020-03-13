c349b9b055c2b0e0d3c277634a1a4da60f6309a3
Arnold Moon <arnold_moon@hotmail.com>
2020-03-13 15:26:12 -0700

    .

--- most_frequent.py
+++ most_frequent.py
@@ -30,6 +30,11 @@ def most_frequent(l):
 
 
 class Node:
+    """
+    modified binary insertion node class for most frequent function.
+    instead of comparing self.data, self.priority will get compared
+    for insertion.
+    """
     def __init__(self, data=None, priority=None, l=None, r=None):
         self.data = data
         self.priority = priority
@@ -51,10 +56,10 @@ class Node:
             if next_node is None:
                 next_node = self.r = Node()
 
-        next_node.append(x, p)
+        return next_node.append(x, p)
 
 
-class Tree:
+class BinaryInsertionSort:
     def __init__(self):
         self.head = Node()
 
@@ -62,20 +67,24 @@ class Tree:
         return self.head.append(x, p)
 
     def traverse(self):
+        """
+        traverse is in-order so lowest priority will be returned first.
+        """
         def _helper(node):
-            r = node.r
-            if node.r is not None:
-                _helper(node.r)
-            result.append(node.data)
             l = node.l
             if node.l is not None:
                 _helper(node.l)
+            result.append(node.data)
+            r = node.r
+            if node.r is not None:
+                _helper(node.r)
 
         if self.head is None:
             return None
         result = []
         _helper(self.head)
         return result
+
 
 def most_frequent_n(l, n):
     """
@@ -95,16 +104,102 @@ def most_frequent_n(l, n):
         current = l_dict.get(i)
         l_dict[i] = count = 1 if current is None else current + 1
 
-    result = Tree()
+    result = BinaryInsertionSort()
     for x, p in l_dict.items():
-        result.append(x, p)
+        # using negative p instead, so to avoid reversed in-order traverse
+        # or reversing the result afterwards.
+        result.append(x, -p)
 
     return result.traverse()[:n]
 
 
-l = [1, 2, 3, 5, 2, 3, 7, 8, 4, 6, 8, 10, 3, 3, 2, 4, 1, 4, 3, 3]
+l = [5, 1, 5, 2, 7, 5, 3, 5, 8, 9, 2, 3]
 print(sorted(set(l), key = lambda X:l.count(X), reverse=True))
 
 print(most_frequent(l))
 print(most_frequent_n(l, 3))
 
+
+
+
+#bonus practice
+def bubble_sort(l):
+    l_len = 0 if l is None else len(l)
+    if l_len < 2:
+        return l
+    while True:
+        done = True
+        i = 0
+        while i < l_len - 1:
+            a, b = l[i], l[i+1]
+            if a > b:
+                done = False
+                l[i+1], l[i] = a, b
+            i += 1
+        if done:
+            break
+    return l
+
+def quick_sort(l):
+    def _helper(x):
+        if x is None or len(x) == 0:
+            return []
+
+        pivot_val = x[0]
+        pivot = []
+        l = []
+        r = []
+        while len(x) > 0:
+            current = x.pop()
+            if current < pivot_val:
+                l.append(current)
+                continue
+            if current > pivot_val:
+                r.append(current)
+                continue
+            pivot.append(current)
+        return _helper(l)\
+               + pivot\
+               + _helper(r)
+
+    return _helper(l)
+
+def quick_sort_inplace(l):
+    def _helper(x, left, right):
+        if left >= right:
+            return
+
+        pivot_val = x[left]
+        p_idx = [left, left]
+        idx = left + 1
+        while idx <= right:
+            current = x[idx]
+
+            if current == pivot_val:
+                if idx > p_idx[1] + 1:
+                    x[idx], x[p_idx[1]+1] = x[p_idx[1]+1], x[idx]
+                p_idx[1] += 1
+                idx += 1
+                continue
+
+            if current < pivot_val:
+                if idx > p_idx[1] + 1:
+                    x[idx], x[p_idx[1]+1] = x[p_idx[1]+1], x[idx]
+                x[p_idx[0]], x[p_idx[1]+1] = x[p_idx[1]+1], x[p_idx[0]]
+                p_idx[0], p_idx[1] = p_idx[0] + 1, p_idx[1] + 1
+            idx += 1
+
+        _helper(x, left, p_idx[0]-1)
+        _helper(x, p_idx[1]+1, right)
+
+    l_len = 0 if l is None else len(l)
+    if l_len < 2:
+        return l
+
+    _helper(l, 0, l_len-1)
+    return l
+
+
+print(l)
+# print(bubble_sort(l))
+print(quick_sort_inplace(l))
