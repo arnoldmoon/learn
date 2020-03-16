85a62d6025a8bbd2b379b77d65e47c623509080a
Arnold Moon <arnold_moon@hotmail.com>
2020-03-15 20:42:01 -0700

    .

--- suggest_word.py
+++ suggest_word.py
@@ -1,29 +1,32 @@
 import string
 
 class Node:
-    def __init__(self, data=None, end=0, c={}):
+    def __init__(self, data=None, word_cnt=0, c={}):
         self.data = data
         self.c = c
-        self.end = end
+        self.word_cnt = word_cnt
 
-    def add_word(self, w, idx=0):
-        if idx == len(w):
-            self.end += 1
+    def add_word(self, w, idx=0, w_len=None):
+        if w_len == None:
+            w_len = len(w)
+        if idx == w_len:
+            self.word_cnt += 1
             return
+
         n_char = w[idx]
         n_node = self.c.get(n_char)
         if n_node is None:
-            n_node = self.c[n_char] = Node(data=n_char, end=0, c={})
-        n_node.add_word(w, idx=idx+1)
+            n_node = self.c[n_char] = Node(data=n_char, word_cnt=0, c={})
+        n_node.add_word(w, idx=idx+1, w_len=w_len)
 
-    def get_word(self, w, idx=-1):
+    def find_node(self, w, idx=-1):
         if idx == len(w) - 1:
             return self
         next_c = w[idx+1]
         next_n = self.c.get(next_c)
         if next_n is None:
             return
-        return next_n.get_word(w, idx=idx+1)
+        return next_n.find_node(w, idx=idx+1)
 
 
 class DocTree:
@@ -31,24 +34,24 @@ class DocTree:
     def char_only(self, w):
         return "".join(c for c in w if c in DocTree.CHAR_SET)
 
-    def traverse(self, node, s=''):
+    def traverse(self, node, prefix=''):
         res = {}
-        def _walk(n, word='', pend=0):
+        def _walk(n, word=''):
             if n is None:
                 return
             
-            if n.end > 0:
-                wset = res.get(n.end)
+            if n.word_cnt > 0:
+                wset = res.get(n.word_cnt)
                 if wset is not None:
                     wset.add(word)
                 else:
                     wset = set([word])
-                    res[n.end] = wset
+                    res[n.word_cnt] = wset
 
             for cn in n.c.values():
-                _walk(cn, word+cn.data)
+                _walk(cn, word=word+cn.data)
 
-        _walk(node, s)
+        _walk(node, word=prefix)
         return res
 
     def __init__(self, doc):
@@ -60,8 +63,8 @@ class DocTree:
         self.head.add_word(self.char_only(w))
 
     def get_word_suggestion(self, w):
-        end_node = self.head.get_word(w)
-        return self.traverse(end_node, w)
+        end_node = self.head.find_node(w)
+        return self.traverse(end_node, prefix=w)
 
 doc_str = 'dog and cat and an animal. any ant andy ann'
 doc_tree = DocTree(doc_str)
